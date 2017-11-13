d96c41f234eb0e82affa1b5474d77836d7cdc816
julianbarnes <julian.barnes100@gmail.com>
2017-11-13 10:26:30 +0900

    Successfully transfers message to server but does not handle persistent connection

--- project.py
+++ project.py
@@ -3,32 +3,31 @@ import sys
 import io
 import argparse
 import os
-
-
-
-if len(sys.argv) <= 1:
-	print('Usage : "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server')
-	sys.exit(2)
 	
 # Create a server socket, bind it to a port and start listening
-
 def main():
 	#Request params from user
 	args = get_params()
 	#Initialize socket 
-	server_socket = socket(AF_INET, SOCK_STREAM)
-	server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+	global client_proxy
+	client_proxy = socket(AF_INET, SOCK_STREAM)
+	client_proxy.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 	#Bind socket to port 
-	server_socket.bind(('',args.port))
+	client_proxy.bind(('',args.port))
 	#Set socket to listen for connection requests
-	server_socket.listen(10)
+	client_proxy.listen(10)
+	print(str(args.port) + " " + str(args.root))
 	
 	while True:
-		# Strat receiving data from the client
+		#Start receiving data from the client
 		print('Ready to serve...')
-		tcpCliSock, addr = tcpSerSock.accept()
-		connect_client(tcpCliSock, args)
-	server_socket.close()
+		global proxy_client
+		proxy_client = client_proxy.accept()[0]
+		request = proxy_client.recv(1024)
+		request_server(request)
+		print(request)
+		#connect_client(proxy_client, args)
+	client_proxy.close()
 	sys.exit()
 		
 def get_params():
@@ -41,57 +40,18 @@ def get_params():
 	args = parser.parse_args()
 	return args
 
-def connect_client(connection, args):
-	print('Received a connection from:', addr)
-		message = connection.recv(1024)
-		print(message)
-		# Extract the filename from the given message
-		print(message.split()[1])
-		filename = message.split()[1].partition("/")[2]
-		print(filename)
-		fileExist = "false"
-		filetouse = "/" + filename
-		print(filetouse)
-		try:
-			# Check wether the file exist in the cache
-			f = open(filetouse[1:], "rb")                      
-			outputdata = f.read()                        
-			fileExist = "true"
-			# ProxyServer finds a cache hit and generates a response message
-			connection.send("HTTP/1.0 200 OK\r\n")            
-			connection.send("Content-Type:text/html\r\n")
-			connection.send(outputdata)
-			
-			print('Read from cache')   
-		# Error handling for file not found in cache
-		except IOError:
-			if fileExist == "false": 
-				# Create a socket on the proxyserver
-				c = # Fill in start.		# Fill in end.
-				hostn = filename.replace("www.","",1)         
-				print(hostn)                                  
-				try:
-					# Connect to the socket to port 80
-					# Fill in start.		
-					# Fill in end.
-					# Create a temporary file on this socket and ask port 80 for the file requested by the client
-					fileobj = c.makefile('r', 0)               
-					fileobj.write("GET "+"http://" + filename + " HTTP/1.0\n\n")  
-					# Read the response into buffer
-					# Fill in start.		
-					# Fill in end.
-					# Create a new file in the cache for the requested file. 
-					# Also send the response in the buffer to client socket and the corresponding file in the cache
-					tmpFile = open("./" + filename,"wb")  
-					# Fill in start.		
-					# Fill in end.			
-				except:
-					print("Illegal request")                                               
-			else:
-				# HTTP response message for file not found
-				# Fill in start.		
-				# Fill in end.
-		# Close the client and the server sockets    
-		tcpCliSock.close() 
-	# Fill in start.		
-	# Fill in end.
+def request_server(request):
+	send_socket = socket()                                  
+	try:
+		#Connect to the socket to port 12000
+		send_socket.connect(('',12000))
+		send_socket.send(request)
+		response = send_socket.recv(1024)
+		proxy_client.send(response)
+		
+	except:
+		print("Could not connect to server")
+	
+main()
+
+
